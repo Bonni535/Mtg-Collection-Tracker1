51aa89f0914de3552c4658b17e1943bcd4ddd5c4
Luca Bonini <luca14bonini@gmail.com>
2023-11-24 14:14:44 -0800

    index.js and collectionCard file added

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,48 @@
-// import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
-// import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
-// import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button, Form } from 'react-bootstrap';
+import { getCollections } from '../api/collectionData';
+import { useAuth } from '../utils/context/authContext';
+import CollectionCard from '../components/cards/collectionCard';
 
 function Home() {
-  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  const [collections, setCollections] = useState([]);
+  const [showingCollections, setShowingCollections] = useState([]);
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+  const { user } = useAuth();
+
+  const getAllTheCollections = () => {
+    getCollections(user.uid).then(setCollections);
+  };
+
+  const handleSearch = (e) => {
+    const searchResults = collections.filter((collection) => collection.name.toLowerCase().includes(e.target.value.toLowerCase()));
+    setShowingCollections(searchResults);
+  };
+
+  useEffect(() => {
+    getAllTheCollections();
+  }, []);
+
+  useEffect(() => {
+    setShowingCollections(collections);
+  }, [collections]);
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
+    <div className="text-center my-4">
+      <Link href="/collections/new" passHref>
+        <Button>Create A New Collection</Button>
+      </Link>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Search</Form.Label>
+        <Form.Control as="textarea" rows={1} onChange={handleSearch} />
+      </Form.Group>
+      <div className="d-flex flex-wrap">
+        {/* TODO: map over collections here using collectionCard component */}
+        {showingCollections?.map((collection) => (
+          <CollectionCard key={collection.firebaseKey} collectionObj={collection} onUpdate={getAllTheCollections} />
+        ))}
+      </div>
     </div>
   );
 }
