dbaec0ceb9afcf7df05b8cf176151a567cfcc6ee
Luca Bonini <luca14bonini@gmail.com>
2023-12-01 17:31:15 -0800

    notes added

--- components/forms/collectionForm.js
+++ components/forms/collectionForm.js
@@ -7,21 +7,28 @@ import { Button } from 'react-bootstrap';
 import { createCollection, updateCollection } from '../../api/collectionData';
 import { useAuth } from '../../utils/context/authContext';
 
+// Initial state: initially set the object that contains the following fields to empy strings.
 const initialState = {
   name: '',
   description: '',
   image: '',
 };
 
+// // The CollectionForm component uses: useState hook to create a variable formInput initialized with initialState.
+// initialState contains the default values we can see above.
+// useRouter is used for routing within the app.
+// useAuth is a custom hook that provides access to authentication informations like the user object.
 function CollectionForm({ obj }) {
   const [formInput, setFormInput] = useState(initialState);
   const router = useRouter();
   const { user } = useAuth();
 
+  // The useEffect listens fot changes in obj and user and updated the form state accordingly.
   useEffect(() => {
     if (obj?.firebaseKey) setFormInput(obj);
   }, [obj, user]);
 
+  // The handleChange function modifies the state by updating the corrisponding field based on user input in the form.
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormInput((prevState) => ({
@@ -30,8 +37,11 @@ function CollectionForm({ obj }) {
     }));
   };
 
+  // The handleSubmit function handles form submission differently based on wheter the obj contains a firebaseKey.
+  // it either updates an existing collection (updateCollection) or creates a new one (createCollection) (if a firebaseKey is found the form updates the relative collection, if not the form creates a new collection).
+  // router.push is used to navigate the user to the home route after updating or creating a collection.
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault(); // this prevents the default behavior of form submission, allowing to handle the submission manually.
     if (obj.firebaseKey) {
       updateCollection(formInput).then(() => router.push('/'));
     } else {
@@ -45,6 +55,11 @@ function CollectionForm({ obj }) {
     }
   };
 
+  // The return statement renders the form with the various input fields and a submit button.
+  // Name Input: renders an input field for the collection name. It binds the value to 'formInput.name' and triggers the handleChange function on change.
+  // Description Input: renders an input field for the collection description. It binds the value to 'formInput.description' and triggers the handleChange function on change.
+  // Image Input: renders an input field for the collection image Url if the user wants to use an image. It expects a valid URL and triggers the handleChange function.
+  // Submit Button: renders the button inside the form, its text dinamically changes based on whether obj contains a firebaseKey or not.If yes -> the button text becomes "Update collection".If not -> the button text becomes "Create collection".
   return (
     <Form onSubmit={handleSubmit}>
       <h2 className="text-white mt-5">{obj?.firebaseKey ? 'Update' : 'Create'} Collection</h2>
@@ -61,7 +76,7 @@ function CollectionForm({ obj }) {
         />
       </FloatingLabel>
 
-      {/* Role INPUT  */}
+      {/* Description INPUT  */}
       <FloatingLabel controlId="floatingInput1" label="Collection Description" className="mb-3">
         <Form.Control
           type="text"
@@ -90,6 +105,10 @@ function CollectionForm({ obj }) {
   );
 }
 
+// Below the propTypes and the defaultProps are being defined for the CollectionForm component.
+// PropTypes.shape: specifies that the obj prop should be an object with specific properties.
+// PropTypes.string: defines the expected data type for each property within the obj object (name, description, image, uid, firebaseKey).
+// This ensure that if obj is provided, it should have properties with string values for 'name', 'description', 'image', 'uid', 'firebaseKey'. It helps catch potential errors or mismatches in the received prop types.
 CollectionForm.propTypes = {
   obj: PropTypes.shape({
     name: PropTypes.string,
@@ -100,8 +119,12 @@ CollectionForm.propTypes = {
   }),
 };
 
+// defaultProps: provides a default value for the obj prop if it's not provided when the CollectionForm component is used.
+// 'obj: initialState': sets the default value of obj to the initialState object (empty strings for 'name', 'description', 'image', 'uid', 'firebaseKey').
+// This ensures that if the 'obj' prop is not passed when using CollectionForm, it will default to an empty state with all properties initialized to empty strings.
 CollectionForm.defaultProps = {
   obj: initialState,
 };
 
+// exporting this component as the default export allows it to be imported and used in other files within the app.
 export default CollectionForm;
